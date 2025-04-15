e495a0c4825bdbe86dc1b5522a494eab9a5051c3
Marcio Marques de Moraes <moraesmm.ds@gmail.com>
2025-04-15 14:53:51 -0300

    Adição da primeira versão

---
+++ Calculo_desconto_diesel_ou_etanol.py
@@ -0,0 +1,43 @@
+#Este código aplica o valor de desconto de acordo com o volume e o tipo de combustível comprado no estabelecimento.
+#Etanol = R$1,70 < 15l --> desconto = 2%/lto | > 15l --> desconto = 4%/lto
+#Diesel = R$2,00 < 15l --> desconto = 3%/lto | > 15l --> desconto = 5%/lto
+
+#Cálculo: 
+#Valor do desconto = Preço/lto x quantidade ltos x porcentagem de desconto
+#Valor ao Cliente = (Preço/lto x quantidade ltos) - Valor do desconto
+
+# Coletamos a quantidade de litros e o tipo de combustível,
+# já deixando o caractere em maiúsculo para facilitar nossa análise
+quantidade_litros = float(input('Informe a quantidade de litros vendidos: '))
+tipo_combustivel = input('Informe o tipo de combustível (E para etanol e D para diesel): ').upper()
+
+#  Verificamos primeiro o tipo de combustível
+if tipo_combustivel == 'E':
+  # Taxamos o valor do preço em litros do etanol
+  preco_litro = 1.70
+  # De acordo com o valor da quantidade de litros, taxamos também o desconto
+  if quantidade_litros <= 15:
+    desconto = 0.02
+  else:
+    desconto = 0.04
+elif tipo_combustivel == 'D':
+  # Taxamos o valor do preço em litros do disel
+  preco_litro = 2.00
+  # De acordo com o valor da quantidade de litros, taxamos também o desconto
+  if quantidade_litros <= 15:
+    desconto = 0.03
+  else:
+    desconto = 0.05
+# Caso ocorra um erro na especificação de tipo de combustível,
+# consideramos entradas inválidas, e os preços são taxados em 0
+else:
+    print('Entradas inválidas!')
+    preco_litro = 0
+    desconto = 0
+
+# Fazemos o cálculo do valor de desconto, seguido do cálculo do preço descontado
+valor_desconto = preco_litro * quantidade_litros * desconto
+valor_pago = preco_litro * quantidade_litros - valor_desconto
+
+# Resultado
+print(f'Valor a ser pago pelo cliente: R$ {valor_pago}')
